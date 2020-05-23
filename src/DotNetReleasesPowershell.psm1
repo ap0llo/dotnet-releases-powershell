2265e9b5b19c92ae8a6375553e1f1ef4c373eb4f
Andreas Gruenwald <andreas@grynwald.net>
2020-05-24 00:47:17 +0200

    Add script to merge all source files into a single-file module

--- src/DotNetReleasesPowershell.psm1
+++ src/DotNetReleasesPowershell.psm1
@@ -1,6 +1,4 @@
-# See https://github.com/dotnet/core/tree/master/release-notes
-$ReleaseIndexUri = "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json"
-
+. (Join-Path $PSScriptRoot "variables.ps1")
 . (Join-Path $PSScriptRoot "model.ps1")
 
 $commandFiles = Get-ChildItem -Path (Join-Path $PSScriptRoot "commands") -Filter "*.ps1"
---
+++ src/variables.ps1
@@ -0,0 +1,2 @@
+# See https://github.com/dotnet/core/tree/master/release-notes
+$ReleaseIndexUri = "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/releases-index.json"
